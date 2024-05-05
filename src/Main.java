b0acafdb3327430ca54c359d3e57a55cb05ce43b
Michel Braga <michel.diener@gmail.com>
2024-05-05 13:58:01 -0300

    Fist commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Conversor de moedas");
+        System.out.println("Digite a opção desejada: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String menu = scanner.nextLine();
+
+        //menu
+        switch (menu){
+            case 1:
+
+
+
+        }
+
+    }
+}
